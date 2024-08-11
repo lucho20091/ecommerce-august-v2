92517d03f1da02c7b870bdb51c2919832ddf1eef
lucho20091 <124323772+lucho20091@users.noreply.github.com>
2024-08-11 18:31:58 -0500

    some progress made

--- src/client/main.js
+++ src/client/main.js
@@ -6,6 +6,9 @@ document.body.addEventListener('click', (e)=>{
 const mainEl = document.querySelector('main')
 const showShopEl = document.getElementById('show-shop')
 const goToHomeEls = document.querySelectorAll('.goToHome')
+const goToShopEls = document.querySelectorAll('.goToShop')
+const homeCardEls = document.querySelectorAll('.third-card')
+const cartEl = document.getElementById('cartEl')
 
 // state
 let home = true
@@ -13,10 +16,18 @@ let shop = false
 let cart = false 
 let checkout = false
 
+
 // Event Listeners
 showShopEl.addEventListener('click', renderShop)
+cartEl.addEventListener('click', renderCart)
 goToHomeEls.forEach(El => {
     El.addEventListener('click', renderHome)
+})
+homeCardEls.forEach(el => {
+    el.addEventListener('click', renderSingleProduct)
+})
+goToShopEls.forEach(el => {
+  el.addEventListener('click', renderShop)
 })
 
 // Render HTML
@@ -26,7 +37,7 @@ function renderShop(){
         <div class="first-shop-background">
           <h1>Shop</h1>
           <div class="location">
-            <p><a href="#">Home</a></p>
+            <p><a href="#" class="goToHome">Home</a></p>
             <img src="/assets/shop/icons/top/dashicons_arrow-down-alt2.png" alt="">
             <p><a href="#">Shop</a></p>
           </div>
@@ -190,17 +201,22 @@ function renderShop(){
         </div>
       </div>`
     window.scrollTo(0,0)
-
+    document.querySelectorAll('.goToHome').forEach(el => {
+      el.addEventListener('click', renderHome)
+    })
+    document.querySelectorAll('.second-shop-card').forEach(el => {
+      el.addEventListener('click', renderSingleProduct)
+    })
 }
 function renderHome(){
     mainEl.innerHTML = `
-    <div class="first-hero">
+          <div class="first-hero">
         <div class="first-background">
           <div class="first-card">
             <p class="first-p">New Arrival</p>
             <h1>Discover Our New Collection</h1>
             <p class="second-p">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
-            <button>BUY NOW</button>
+            <button class="goToShop">BUY NOW</button>
           </div>
         </div>
       </div>
@@ -286,7 +302,7 @@ function renderHome(){
             <h3>50+ Beautiful rooms 
               inspiration</h3>
             <p>Our designer already made a lot of beautiful prototipe of rooms that inspire you</p>
-            <button class="fourth-btn">Explore More</button>
+            <button class="fourth-btn goToShop">Explore More</button>
           </div>
           <div class="fourth-right">
             <div class="slide">
@@ -335,7 +351,325 @@ function renderHome(){
       </div>`
       window.scrollTo(0,0)
       document.getElementById('show-shop').addEventListener('click', renderShop)
+      document.querySelectorAll('.third-card').forEach(el => {
+        el.addEventListener('click', renderSingleProduct)
+    })
+      document.querySelectorAll('.goToShop').forEach(el => {
+        el.addEventListener('click', renderShop)
+      })
+    }
+function renderSingleProduct(){
+  mainEl.innerHTML = `
+  <div class="navigation-single-product">
+        <div class="location">
+          <p><a href="#" class="goToHome">Home</a></p>
+          <img src="/assets/shop/icons/top/dashicons_arrow-down-alt2.png" alt="">
+          <p><a href="#" class="goToShop">Shop</a></p>
+          <img src="/assets/shop/icons/top/dashicons_arrow-down-alt2.png" alt="">
+          <p><a href="#">Product</a></p>
+        </div>
+      </div>
+      <div class="first-single-product">
+        <div class="first-single-1">
+          <img src="/assets/singleProduct/images/top/Group 94.png" alt="">
+          <img src="/assets/singleProduct/images/top/Group 96.png" alt="">
+          <img src="/assets/singleProduct/images/top/Group 97.png" alt="">
+          <img src="/assets/singleProduct/images/top/Group 98.png" alt="">
+        </div>
+        <div class="first-single-2">
+          <div class="first-single-2-background">
+            <img src="/assets/singleProduct/images/top/Asgaard sofa 3.png" alt="">
+          </div>
+        </div>
+        <div class="first-single-3">
+          <h1>Asgaard sofa</h1>
+          <h2>Rs. 250,000.00</h2>
+          <img src="/assets/singleProduct/images/top/Group 88.png" alt="">
+          <span>5 customer Review</span>
+          <p class="small-description">Setting the bar as one of the loudest speakers in its class, the Kilburn is a compact, stout-hearted hero with a well-balanced audio which boasts a clear midrange and extended highs for a sound.
+          </p>
+          <label for="size-product">Size</label>
+          <input type="radio" name="size-product" value="l">
+          <input type="radio" name="size-product" value="xl">
+          <input type="radio" name="size-product" value="xs">
+          <label for="color-product">Color</label>
+          <input type="radio" name="color-product" value="pink">
+          <input type="radio" name="color-product" value="black">
+          <input type="radio" name="color-product" value="yellow">
+          <div class="quantity">
+            <button>-</button>
+            <p>1</p>
+            <button>+</button>
+          </div>
+          <p>SKU : SS001</p>
+          <p>Category : Sofas</p>
+          <p>Tags : </p>
+          <button id="add-to-cart" class="addToCart">Add To Cart</button>
+        </div>
+      </div>
+      <div class="second-single-product">
+        <div class="second-single-product-a-elements">
+          <a href="#">Description</a>
+          <a href="#">Additional Information</a>
+          <a href="#">Reviews [5]</a>
+        </div>
+        <div class="description-mid">
+          <p>Embodying the raw, wayward spirit of rock ‘n’ roll, the Kilburn portable active stereo speaker takes the unmistakable look and sound of Marshall, unplugs the chords, and takes the show on the road.</p>
+          <p>Weighing in under 7 pounds, the Kilburn is a lightweight piece of vintage styled engineering. Setting the bar as one of the loudest speakers in its class, the Kilburn is a compact, stout-hearted hero with a well-balanced audio which boasts a clear midrange and extended highs for a sound that is both articulate and pronounced. The analogue knobs allow you to fine tune the controls to your personal preferences while the guitar-influenced leather strap enables easy and stylish travel.</p>
+        </div>
+        <div class="images-mid">
+          <img src="/assets/singleProduct/images/mid/Group 106.png" alt="">
+          <img src="/assets/singleProduct/images/mid/Group 107.png" alt="">
+        </div>
+      </div>
+      <div class="third-single-product">
+        <h3>Related Products</h3>
+        <div class="third-single-product-cards">
+          <div class="third-single-product-card">
+            <img src="/assets/homepage/images/second-section/image 2.png" alt="">
+            <h3 class="title-item">Syltherine</h3>
+            <p class="mini-description">Stylish cafe chair</p>
+            <p class="price">Rp 2.500.000 <span class="price-before">Rp 3.500.000</span></p>
+          </div>
+          <div class="third-single-product-card">
+            <img src="/assets/homepage/images/second-section/image 2.png" alt="">
+            <h3 class="title-item">Syltherine</h3>
+            <p class="mini-description">Stylish cafe chair</p>
+            <p class="price">Rp 2.500.000 <span class="price-before">Rp 3.500.000</span></p>
+          </div>
+          <div class="third-single-product-card">
+            <img src="/assets/homepage/images/second-section/image 2.png" alt="">
+            <h3 class="title-item">Syltherine</h3>
+            <p class="mini-description">Stylish cafe chair</p>
+            <p class="price">Rp 2.500.000 <span class="price-before">Rp 3.500.000</span></p>
+          </div>
+          <div class="third-single-product-card">
+            <img src="/assets/homepage/images/second-section/image 2.png" alt="">
+            <h3 class="title-item">Syltherine</h3>
+            <p class="mini-description">Stylish cafe chair</p>
+            <p class="price">Rp 2.500.000 <span class="price-before">Rp 3.500.000</span></p>
+          </div>
+        </div>
+        <button class="show-more-btn goToShop">Show More</button>
+      </div>
+  `
+  window.scrollTo(0,0)
+  document.querySelectorAll('.goToHome').forEach(el => {
+    el.addEventListener('click', renderHome)
+  })
+  document.querySelectorAll('.goToShop').forEach(el => {
+    el.addEventListener('click', renderShop)
+  })
+  document.querySelectorAll('.third-single-product-card').forEach(el => {
+    el.addEventListener('click', renderSingleProduct)
+  })
 }
+function renderCart(){
+  mainEl.innerHTML = `
+      <div class="first-cart">
+        <div class="first-cart-background">
+          <h1>Cart</h1>
+          <div class="location">
+            <p><a href="#" class="goToHome">Home</a></p>
+            <img src="/assets/shop/icons/top/dashicons_arrow-down-alt2.png" alt="">
+            <p><a href="#">Cart</a></p>
+          </div>
+        </div>
+      </div>
 
+      <div class="second-cart">
+        <div class="second-cart-background">
+          <div class="second-cart-left">
+            <div class="top">
+              <p>Product</p>
+              <p>Price</p>
+              <p>Quantity</p>
+              <p>Subtotal</p>
+            </div>
+            <div class="mid">
+              <img src="/assets/cart/images/mid/Group 146.png" alt="">
+              <p>Asgaard sofa</p>
+              <p>Rs. 250,000.00</p>
+              <input type="number" value="1">
+              <p>Rs. 250,000.00</p>
+              <button><img src="/assets/cart/icon/mid/ant-design_delete-filled.png" alt=""></button>
+            </div>
+          </div>
+          <div class="second-cart-right">
+            <div class="right-box">
+              <h2>Cart Totals</h2>
+              <div class="second-cart-right-div">
+                <p>Subtotal <span class="sub-span">RS. 250,000.00</span></p>
+                <p>Total <span class="tot-span">RS. 250,000.00</span></p>
+              </div>
+              <a href="#" id="checkoutBtn">Check Out</a>
+            </div>
+          </div>
+        </div>
+      </div>
 
+      <div class="third-cart">
+        <div class="third-cart-banners">
+          <div class="banner1">
+            <img src="/assets/shop/icons/mid/trophy 1.png" alt="">
+            <div class="banner-text">
+              <h3>Hight Quality</h3>
+              <h4>crafted from top materials</h4>
+            </div>
+          </div>
+          <div class="banner2">
+            <img src="/assets/shop/icons/mid/guarantee.png" alt="">
+            <div class="banner-text">
+              <h3>Warranty Protection</h3>
+              <h4>Over 2 years</h4>
+            </div>
+          </div>
+          <div class="banner1">
+            <img src="/assets/shop/icons/mid/shipping.png" alt="">
+            <div class="banner-text">
+              <h3>Free Shipping</h3>
+              <h4>Order over 150$</h4>
+            </div>
+          </div>
+          <div class="banner1">
+            <img src="/assets/shop/icons/mid/customer-support.png" alt="">
+            <div class="banner-text">
+              <h3>24/7 Support</h3>
+              <h4>Dedicated support</h4>
+            </div>
+          </div>
+      </div>
+      </div>`
+  window.scrollTo(0,0)
+  document.querySelectorAll('.goToHome').forEach(el => {
+    el.addEventListener('click', renderHome)
+  })
+  document.querySelectorAll('.goToShop').forEach(el => {
+    el.addEventListener('click', renderShop)
+  })
+  document.querySelectorAll('.third-single-product-card').forEach(el => {
+    el.addEventListener('click', renderSingleProduct)
+  })
+  document.getElementById('checkoutBtn').addEventListener('click', renderCheckout)
+}
+function renderCheckout(){
+  mainEl.innerHTML = `
+      <div class="first-checkout">
+        <div class="first-checkout-background">
+          <h1>Checkout</h1>
+          <div class="location">
+            <p><a href="#" class="goToHome">Home</a></p>
+            <img src="/assets/shop/icons/top/dashicons_arrow-down-alt2.png" alt="">
+            <p><a href="#">Checkout</a></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="second-checkout">
+        <div class="second-checkout-background">
+          <div class="second-checkout-left">
+            <form>
+              <h2>Billing details</h2>
+              <div class="name">
+                <div class="firstname">
+                  <label for="firstname">First Name</label>
+                  <input type="text" name="firstname" id="firstname">
+                </div>
+                <div class="lastname">
+                  <label for="lastname">Last Name</label>
+                  <input type="text" name="lastname" id="lastname">
+                </div>
+              </div>
+              <label for="companyname">Company Name (Optional)</label>
+              <input type="text" name="companyname" id="companyname">
+              <label for="country">Country / Region</label>
+              <input type="text" name="country" id="country">
+              <label for="address">Street address</label>
+              <input type="text" name="address" id="address">
+              <label for="town">Town / City</label>
+              <input type="text" name="town" id="town">
+              <label for="zip-code">Zip code</label>
+              <input type="text" name="zip-code" id="zip-code">
+              <label for="phone">Phone</label>
+              <input type="text" name="phone" id="phone">
+              <label for="email">Email address</label>
+              <input type="text" name="email" id="email">
+              <input type="text" name="additional" id="additional" placeholder="Additional Information">
+            </form>
+          </div>
+          <div class="second-checkout-right">
+            <div class="top">
+              <div class="flex title-checkout">
+                <p>Product</p>
+                <p>Subtotal</p>
+              </div>
+              <div class="flex">
+                <p>Asgaard sofa x 1</p>
+                <p>Rs. 250,000.00</p>
+              </div>
+              <div class="flex">
+                <p>Subtotal</p>
+                <p>Rs. 250,000.00</p>
+              </div>
+              <div class="flex">
+                <p>Total</p>
+                <p class="total-checkout">Rs. 250,000.00</p>
+              </div>
+            </div>
+            <div class="mid">
+              <input type="radio">
+              <p>Make your payment directly into our bank account. Please use your Order ID as the payment reference. Your order will not be shipped until the funds have cleared in our account.</p>
+              <input type="radio">
+              <input type="radio">
+              <p>Your personal data will be used to support your experience throughout this website, to manage access to your account, and for other purposes described in our privacy policy.</p>
+              <button>Place order</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="third-checkout">
+        <div class="third-checkout-banners">
+          <div class="banner1">
+            <img src="/assets/shop/icons/mid/trophy 1.png" alt="">
+            <div class="banner-text">
+              <h3>Hight Quality</h3>
+              <h4>crafted from top materials</h4>
+            </div>
+          </div>
+          <div class="banner2">
+            <img src="/assets/shop/icons/mid/guarantee.png" alt="">
+            <div class="banner-text">
+              <h3>Warranty Protection</h3>
+              <h4>Over 2 years</h4>
+            </div>
+          </div>
+          <div class="banner1">
+            <img src="/assets/shop/icons/mid/shipping.png" alt="">
+            <div class="banner-text">
+              <h3>Free Shipping</h3>
+              <h4>Order over 150$</h4>
+            </div>
+          </div>
+          <div class="banner1">
+            <img src="/assets/shop/icons/mid/customer-support.png" alt="">
+            <div class="banner-text">
+              <h3>24/7 Support</h3>
+              <h4>Dedicated support</h4>
+            </div>
+          </div>
+      </div>
+      </div>
+  `
+  window.scrollTo(0,0)
+  document.querySelectorAll('.goToHome').forEach(el => {
+    el.addEventListener('click', renderHome)
+  })
+  document.querySelectorAll('.goToShop').forEach(el => {
+    el.addEventListener('click', renderShop)
+  })
+  document.querySelectorAll('.third-single-product-card').forEach(el => {
+    el.addEventListener('click', renderSingleProduct)
+  })
+}
 // Functionality
